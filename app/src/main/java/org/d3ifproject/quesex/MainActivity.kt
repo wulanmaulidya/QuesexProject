d72771df46da702229d3c9d5a20a833805b6e81b
yennas23 <yennasopia23@gmail.com>
2023-05-14 20:49:09 +0700

    fixed main activity(yenna sopia)

--- app/src/main/java/org/d3ifproject/quesex/MainActivity.kt
+++ app/src/main/java/org/d3ifproject/quesex/MainActivity.kt
@@ -1,7 +1,8 @@
 package org.d3ifproject.quesex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
