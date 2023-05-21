cfdd23c4221d1d22b91a6b18473283e9a58c6b07
Wulan <maulidyawulan@student.telkomuniversity.ac.id>
2023-05-21 17:44:29 +0700

    perbaikan pt 2

--- app/src/main/java/org/d3ifproject/quesex/SplashActivity.kt
+++ app/src/main/java/org/d3ifproject/quesex/SplashActivity.kt
@@ -1,4 +1,43 @@
 package org.d3ifproject.quesex
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // melakukan handler jika os android versi android R
+        // karena flag_fullscreen deprecated pada android R
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            // buat fullscreen dan nutupin status bar pada android
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashTime: Long = 3000 // lama splash (3000 = 3 detik)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTime)
+    }
 }
