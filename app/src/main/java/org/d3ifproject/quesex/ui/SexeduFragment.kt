309b45be2fab3dc91d07609fcfd6514d8ca48f1d
Wulan <maulidyawulan@student.telkomuniversity.ac.id>
2023-05-28 16:50:59 +0700

    perbaikan pt 2

--- app/src/main/java/org/d3ifproject/quesex/ui/SexeduFragment.kt
+++ app/src/main/java/org/d3ifproject/quesex/ui/SexeduFragment.kt
@@ -24,8 +24,8 @@ class SexeduFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.cardPubertas.setOnClickListener {
             val gambar = R.drawable.pubertas
-            val judul = "@string/judul_pubertas" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_pubertas" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_pubertas// WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_pubertas // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
@@ -33,56 +33,56 @@ class SexeduFragment : Fragment() {
 
         binding.cardIdGender.setOnClickListener {
             val gambar = R.drawable.identitas_gender
-            val judul = "@string/judul_gender" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_gender" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_gender // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_gender // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
         }
         binding.cardOrientasiSex.setOnClickListener {
             val gambar = R.drawable.orientasi_sex
-            val judul = "@string/judul_orientasi_sex" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_orientasi_sex" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_orientasi_sex // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_orientasi_sex // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
         }
         binding.cardPms.setOnClickListener {
             val gambar = R.drawable.pms
-            val judul = "@string/judul_pms" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_pms" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_pms // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_pms // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
         }
         binding.cardKehamilan.setOnClickListener {
             val gambar = R.drawable.kehamilan
-            val judul = "@string/judul_kehamilan" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_kehamilan" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_kehamilan // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_kehamilan // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
         }
         binding.cardTubuh.setOnClickListener {
             val gambar = R.drawable.tubuh_kita
-            val judul = "@string/judul_tubuh" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_tubuh" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_tubuh // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_tubuh // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
         }
         binding.cardReproduksi.setOnClickListener {
             val gambar = R.drawable.reproduksi
-            val judul = "@string/judul_reproduksi" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_reproduksi" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_reproduksi // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_reproduksi // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
         }
         binding.cardHubungan.setOnClickListener {
             val gambar = R.drawable.hubungan
-            val judul = "@string/judul_hubungan" // WAJIB DI MASUKIN STRING!
-            val isi = "@string/isi_hubungan" // WAJIB DI MASUKIN STRING!
+            val judul = R.string.judul_hubungan // WAJIB DI MASUKIN STRING!
+            val isi = R.string.isi_hubugan // WAJIB DI MASUKIN STRING!
 
             val passData = SexeduFragmentDirections.actionSexeduFragmentToSexeduDetailFragment(gambar,judul,isi)
             findNavController().navigate(passData)
